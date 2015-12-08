b0cbdd88b54bcbe5212ccb0dca3a31d2b8fc7f2e
Ed Glazenski <edglazenski@gmail.com>
2015-12-08 12:02:53 -0500

    Added animation on view change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 (function() {
-  var app = angular.module('myApp', ['ngMessages', 'ngRoute']);
+  var app = angular.module('myApp', ['ngMessages', 'ngRoute', 'ngAnimate']);
 
   app.config(function($routeProvider) {
       $routeProvider.when('/', {
@@ -27,74 +27,45 @@
   app.controller('MealCtrl', function($rootScope) {
       this.submit = function() {
         //Calculates subtotal from base price and tax %
-        this.subTotal = parseInt(this.price) + (parseInt(this.price) * (parseInt(this.tax) / 100));
+        $rootScope.subTotal = parseInt($rootScope.price) + (parseInt($rootScope.price) * (parseInt($rootScope.tax) / 100));
         
         //Calculates tip cost
-        this.tipTotal = parseInt(this.price) * (parseInt(this.tip) / 100);
+        $rootScope.tipTotal = parseInt($rootScope.price) * (parseInt($rootScope.tip) / 100);
 
         //Calculates total cost of tip + subtotal
-        this.total = this.subTotal + this.tipTotal;
+        $rootScope.total = $rootScope.subTotal + $rootScope.tipTotal;
 
-        $rootScope.tipsCount+= this.tipTotal;
+        $rootScope.tipsCount+= $rootScope.tipTotal;
         $rootScope.mealsCount++;
         $rootScope.avgTip = $rootScope.tipsCount / $rootScope.mealsCount;
+
+        this.clearInputs();
       };
 
       $rootScope.tipsCount = 0;
       $rootScope.mealsCount = 0;
-  });
-
-  app.controller('EarningsCtrl', function() {
-
-  });
-
-
-
-
-
-
-
-
-  app.controller('CalcCtrl', ['$scope', function($scope) {
-      
-      
-     
-      //On submit performs calculations for Customer Charges and My Earnings
-      $scope.submit = function() {
-        
-
-        
-
-        //Calculates avg tip
-        $scope.avgTip = $scope.tipsCount / $scope.mealsCount;
-
-        //Calls function to clear input fields
-        $scope.clearInputs();
-
-        
-
-      };
 
       //Clears form input fields
-      $scope.clearInputs = function() {
-        $scope.price = '';
-        $scope.tax = '';
-        $scope.tip = '';
+      this.clearInputs = function() {
+        $rootScope.price = '';
+        $rootScope.tax = '';
+        $rootScope.tip = '';
       };
+  });
 
+  app.controller('EarningsCtrl', function($rootScope) {
       //Resets all data
-      $scope.clearData = function() {
-        $scope.subTotal = null;
-        $scope.tipTotal = null;
-        $scope.total = null;
+      this.clearData = function() {
+        $rootScope.subTotal = null;
+        $rootScope.tipTotal = null;
+        $rootScope.total = null;
 
-        $scope.tipsCount = 0;
-        $scope.mealsCount = 0;
-        $scope.avgTip = 0; 
-
-        $scope.clearInputs();
+        $rootScope.tipsCount = 0;
+        $rootScope.mealsCount = 0;
+        $rootScope.avgTip = 0; 
 
       };
-  }]);
+
+  });
 
 })();
